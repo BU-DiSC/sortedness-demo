91bb8e05f57c5220e5a5925b7cb7c0538ed26e8c
cangokmen <47925886+cangokmen@users.noreply.github.com>
2024-10-03 13:05:34 -0400

    Cleaned up the code, moved js to seperate files

--- src/data.js
+++ src/data.js
@@ -1,33 +1,31 @@
-
-const shuffleArray = array => {
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      const temp = array[i];
-      array[i] = array[j];
-      array[j] = temp;
-    }
-  };
-
-export function generate_data(N, sorted = true)
+function generate_data(N, sorted = true)
 {
+    const shuffleArray = array => {
+      for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        const temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+      }
+    };
     // our API expects data to be an array of [key, value] pairs
     // where key is the unique identifier for the data point
     // and value is the value of the data point
-    let data = [];
+    let data_points = [];
     for(let i = 1; i <= N; i++)
     {
-        data.push(i);
+        data_points.push(i);
     }
 
     // copy data and shuffle it randomly 
-    let shuffled_data = data.slice();
+    let shuffled_data = data_points.slice();
     shuffleArray(shuffled_data);
 
     let plot_data = [];
-    plot_data.push(['x', 'y']);
+    plot_data.push(['Position', 'Value']);
     for(let i = 0; i < N; i++)
     {
-        plot_data.push([i, sorted ? data[i] : shuffled_data[i]]);
+        plot_data.push([parseInt(data_points[i]), parseInt(sorted ? data_points[i] : shuffled_data[i])]);
     }
 
     return plot_data;
