d1c210513e336b4c52baf84be13af7ce05e355d6
cangokmen <47925886+cangokmen@users.noreply.github.com>
2025-04-23 20:30:12 -0400

    Made the control buttons look better, and fixed reset issue in inversions

--- src/js/draw.js
+++ src/js/draw.js
@@ -836,6 +836,7 @@ function reset() {
     resetDropdown(document.getElementById("cmp-select-K"), "K");
     resetDropdown(document.getElementById("cmp-select-L"), "L");
     resetDropdown(document.getElementById("cmp-select-B"), "B");
+    resetDropdown(document.getElementById("cmp-select-I"), "I");
 
     // Hide elements that should not be visible initially
     document.getElementById("chart-column").classList.add("hidden");
